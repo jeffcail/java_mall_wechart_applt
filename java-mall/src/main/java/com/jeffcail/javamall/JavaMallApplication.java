eefa95e0260f5168bde931269223d46832bef799
jeffcail <cljcailianjie@163.com>
2023-06-04 20:00:12 +0800

    springboot框架搭建

---
+++ java-mall/src/main/java/com/jeffcail/javamall/JavaMallApplication.java
@@ -0,0 +1,13 @@
+package com.jeffcail.javamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMallApplication.class, args);
+    }
+
+}
---
+++ java-mall/src/main/java/com/jeffcail/javamall/controller/PingController.java
@@ -0,0 +1,23 @@
+package com.jeffcail.javamall.controller;
+
+import com.jeffcail.javamall.Result.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName PingController
+ * @Description TODO
+ * @Author cc
+ * @Date 2023/6/4 7:53 下午
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/api")
+public class PingController {
+
+    @GetMapping("/ping")
+    R test() {
+        return R.ok("pong...");
+    }
+}
