96217a73c7d37cda17dc10e43c319927b620c247
jeffcail <cljcailianjie@163.com>
2023-06-07 15:40:10 +0800

    springboot拦截器映射本地资源

---
+++ java-mall/src/main/java/com/jeffcail/javamall/config/WebAppConfigurer.java
@@ -0,0 +1,24 @@
+package com.jeffcail.javamall.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName WebAppConfigurer
+ * @Description TODO
+ * @Author cc
+ * @Date 2023/6/7 2:38 下午
+ * @Version 1.0
+ */
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/image/swiper/**").
+                addResourceLocations("file:/Users/cc/project/github/java/java_mall_wechat_applt/java-mall/swiperImgs/");
+    }
+}
