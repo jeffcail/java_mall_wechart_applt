1c59cddf372ea02111daac4f2d616ea7bebd7532
jeffcail <cljcailianjie@163.com>
2023-06-07 23:48:04 +0800

    分类页面实现

--- mall_wechat_applt/pages/category/index.js
+++ mall_wechat_applt/pages/category/index.js
@@ -1,3 +1,6 @@
+import {request,getBaseUrl} from "../../utils/request.js"
+import regeneratorRuntime from "../../lib/runtime/runtime.js"
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +8,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList:[],
+    // 右侧商品数据
+    rightProductList:[],
+    baseUrl: '',
+    currentIndex: 0 // 当前选中左侧菜单的索引
   },
+  // 所有数据
+  Cates:[],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    const baseUrl = getBaseUrl();
+    this.setData({
+      baseUrl
+    })
 
+    this.getCatesList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+   // 分类数据
+   async getCatesList() {
+    const result = await request({
+      url: "/api/bigType/findCategories",
+      method: "GET"
+    });
+    this.Cates = result.message;
+    let leftMenuList = this.Cates.map((v) => {
+      return v.name
+    })
+    let rightProductList = this.Cates[0].smallTypeList;
+     this.setData({
+       leftMenuList,
+       rightProductList
+     })   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  handleMenuChange(e) {
+    const {index} = e.currentTarget.dataset;
+    let rightProductList = this.Cates[index].smallTypeList;
+    this.setData({
+      currentIndex: index,
+      rightProductList
+    })
   }
 })
